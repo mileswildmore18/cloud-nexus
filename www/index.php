c386b752eb601fee8d0d61038a047342ccc6f199
Miles Wildmore <mileswildmore18@gmail.com>
2024-09-09 10:55:50 -0400

    Moved the CSS code over to the assets-css folder. Also added the bootstrap javascript to make the carousel functional along with making the background the color desired.

--- www/index.php
+++ www/index.php
@@ -9,6 +9,16 @@
 
 <div class="container">
   <!-- Content here -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://getbootstrap.com/docs/5.3/assets/css/docs.css"
+      rel="stylesheet"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  
   <div class="row">
     <div class="col-sm-12">
       <div class="section-header text-center">
@@ -21,12 +31,12 @@
     <div class="col-sm-12">
       <div id="customers" class="carousel slide">
         <div class="carousel-indicators">
-          <button type="button" data-bs-target="#customers" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#customers" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#customers" data-bs-slide-to="0"  aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#customers" data-bs-slide-to="1" class="active" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#customers" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
-          <div class="carousel-item active">
+          <div class="carousel-item">
             <div class="icon-area">
               <i class="customer customer-comments"></i>
             </div>
@@ -39,7 +49,7 @@
               <h6>Front-End Web Developer</h6>
             </div>
           </div>
-          <div class="carousel-item">
+          <div class="carousel-item active">
             <div class="icon-area">
               <i class="customer customer-comments"></i>
             </div>
@@ -82,128 +92,5 @@
     </div>
   </div>
 </div>
-   <style>
-
-    body {
-      background-color: #36628c;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-    }
-
-    .comment {
-      color: white;
-      text-align: center;
-      font-size: 20px;
-
-    }
-    
-    .testimonial .container {
-      width: 100%;
-      margin: auto;
-    }
-
-    .border {
-      width: 160px;
-      height: 5px;
-      background: white;
-      margin: 26px auto;
-    }
-
-    .industry {
-      display: flex;
-      justify-content: center;
-      color: white;
-      font-weight: bold;
-    }
-
-    .section-header h1{
-      font-weight: 600;
-      color: white;
-      padding-bottom: 10px;
-    }
-
-    .icon-area {
-      text-align: center;
-    }
-
-    .icon-area i {
-      color: white;
-      font-size: 80px;
-      margin-bottom: 20px;
-    }
-    .content p{
-      color: white;
-      margin: auto;
-      width: 60%;
-    }
-
-    .person{
-      width: 100px;
-      height: 100px;
-      display: block;
-      margin: auto;
-      border-radius: 50%;
-      overflow: hidden;
-      border: 5px solid #0c2440;
-      margin-top: 50px;
-    }
-
-    .person img{
-      width: 100%;
-    }
-
-
-    .content h5 {
-      color:white;
-      font-weight: 800;
-      margin-top: 15px;
-    }
-
-    .content h6 {
-      color: white;
-    }
-
-    .carousel-indicators{
-      bottom: -80px;
-    }
-
-    .carousel-indicators [data-bs-target]{
-      width: 60px;
-      height: 10px;
-      background-color: white;
-    }
-
-    .carousel-control-prev {
-      padding-top: 250px;
-      left: 0;
-      background-color: #36628C;
-      height: 30px;
-      color: white;
-      border: none;
-      
-    }
-
-    .carousel-control-next {
-    padding-top: 250px;
-    right: 0;
-    background-color: #36628C;
-    height: 30px;
-    color: white;
-    border: none;
-}
-    @media (max-width: 768px) and (max-width: 991 px){
-      .section-header p{
-        width: 85%;
-      }
-      .content p{
-        width: 95%;
-      }
-    }
-
-    @media (max-width: 767px){
-
-    }
-    </style>
-
+   
 <?php include_once('./footer.php'); ?>
